f1c14013994a563fba30369124952977c3c0911a
marikell <marikell_silva@hotmail.com>
2019-05-15 01:08:49 -0300

    Alterações no Rasa para buscar em API as respostas

--- containers/eve-rasa/actions/actions.py
+++ containers/eve-rasa/actions/actions.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from api.api_config import ApiConfig
 from rasa_core_sdk import Action
 
 class SubscribeAction(Action):
@@ -7,6 +8,27 @@ class SubscribeAction(Action):
     return "action_subscribe"
 
   def run(self, dispatcher, tracker, domain):
-    text = 'Okay! Now you are subscribed'
-    dispatcher.utter_message(text) #send the message back to the user
-    return []
+    intent = tracker.latest_message["intent"].get("name")
+    entities = tracker.latest_message.get("entities", [])
+    # entity = {e["entity"]: e["value"] for e in entities}[0]
+
+    if intent is None:
+      intent = ""
+    
+    if len(entities) == 0:
+      entities = []
+
+    # url = ApiConfig().get_action_answer_URL()+ "?intent={}&entity={}".format(intent,entity)
+    
+
+    # entities_json = json.dumps(entities)
+    # entity = getattr(domain,"entities").first()
+    # intent = getattr(domain,"intents").first()
+    # entity = tracker.latest_message["entities"][0]["value"]
+    # entity = tracker.latest_message["entities"]
+    # print(entity)
+    # intent = tracker.latest_message["intent"]["name"]
+    # r = requests.get(url)
+
+    dispatcher.utter_message(intent) #send the message back to the user
+    return
