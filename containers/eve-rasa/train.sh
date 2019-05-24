8afa5e4856a6bb9ddae7148b502f824d130da9e4
marikell <marikell_silva@hotmail.com>
2019-05-24 12:22:35 -0300

    Deixando fixo o nome do model gerado pelo Rasa

--- containers/eve-rasa/train.sh
+++ containers/eve-rasa/train.sh
@@ -1,3 +1,3 @@
 echo "[EVE] training rasa model"
 
-docker run --rm -v $(pwd):/app rasa/rasa:latest-full train --domain domain.yml --data data --out models
+docker run --rm -v $(pwd):/app rasa/rasa:latest-full train --domain domain.yml --data data --fixed-model-name core --out models
