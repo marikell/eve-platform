bec018df3bbecbbb3ac0db72fa26dd5156bbd2ee
marikell <marikell_silva@hotmail.com>
2019-05-24 14:03:00 -0300

    [E1-27] Alterações na API do flask para chamadas do Rasa

--- containers/eve-api/src/config/database_config.py
+++ containers/eve-api/src/config/database_config.py
@@ -4,3 +4,8 @@ DEV_MONGO_CONFIGURATION = {
     'DATABASE_NAME': 'evedb',
     'DATABASE_URL': 'mongodb://eve_mongo:27017/evedb'
 }
+
+PROD_MONGO_CONFIGURATION ={
+    'DATABASE_NAME': '<dbname>',
+    'DATABASE_URL': 'mongodb://<username>:<password>@<clustername>-shard-00-00-90nlz.mongodb.net:27017,<clustername>-shard-00-01-90nlz.mongodb.net:27017,<clustername>-shard-00-02-90nlz.mongodb.net:27017/<dbname>?ssl=true&replicaSet=EveCluster-shard-0&authSource=<authsource>&retryWrites=true'
+} 
--- containers/eve-api/src/services/generic_service.py
+++ containers/eve-api/src/services/generic_service.py
@@ -1,7 +1,7 @@
 from mongoengine import connect
-from config.database_config import DEV_MONGO_CONFIGURATION
+from config.database_config import PROD_MONGO_CONFIGURATION
 
 class GenericService():
     def __init__(self):
-        connect(DEV_MONGO_CONFIGURATION['DATABASE_NAME'], host=DEV_MONGO_CONFIGURATION['DATABASE_URL'])
+        connect(PROD_MONGO_CONFIGURATION['DATABASE_NAME'], host=PROD_MONGO_CONFIGURATION['DATABASE_URL'])
     
