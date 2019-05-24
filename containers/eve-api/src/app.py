a73a974207e931d58b4ad60e8162dd7c73ba85e4
marikell <marikell_silva@hotmail.com>
2019-05-24 12:49:51 -0300

    [E1-18] Subir Flask API no Heroku

--- containers/eve-api/src/app.py
+++ containers/eve-api/src/app.py
@@ -4,8 +4,11 @@ from werkzeug import Response
 from blueprints.entity_intent_answer_blueprint import app_entity_intent_answer
 from blueprints.answer_blueprint import app_answer
 from flask_api import FlaskAPI
+import os
 
 app = FlaskAPI(__name__)
+
+port = 5001
 
 def register_blueprints(app):
     app.register_blueprint(app_answer)
@@ -13,8 +16,9 @@ def register_blueprints(app):
 
 @app.route('/', methods=['GET'])
 def get():
-    return Response('Flask is running in port 5001')
+    return Response('Flask is running in port {}'.format(port))
     
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5001))
     register_blueprints(app)
-    app.run(debug=True, host='0.0.0.0', port='5001')
+    app.run(debug=True, host='0.0.0.0', port=port)
