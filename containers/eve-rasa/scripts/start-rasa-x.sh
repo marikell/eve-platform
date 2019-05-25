7a77d93f8d9ce5e14c79b92f1d1ed9d6c4c43afd
marikell <marikell_silva@hotmail.com>
2019-05-24 22:07:23 -0300

    Script do Rasa X apontando para o local-endpoints.yml

--- containers/eve-rasa/scripts/start-rasa-x.sh
+++ containers/eve-rasa/scripts/start-rasa-x.sh
@@ -3,6 +3,7 @@ echo "Confira se o arquivo endpoints.yml está com as configurações desejadas.
 if [ ! -d "env" ]; then
     python3 -m virtualenv env
 fi
+docker stop eve_core
 source env/bin/activate
 pip install rasa-x --extra-index-url https://pypi.rasa.com/simple
-rasa x
+rasa x --endpoints ./dev-endpoints/local-endpoints.yml
